32e4ae70ad3e50a6c35bccf2d53c82754fc0abf7
GagarinYegor <yegorgagarin@mail.ru>
2025-03-10 01:28:48 +0500

    ->

---
+++ client4.cpp
@@ -0,0 +1,25 @@
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <cstring>
+
+struct mbuf{
+  long mtype;
+  char mtext[100] = "";
+}mobj;
+
+int main(){
+  int fd, i, key;
+  key = ftok("queue", 1);
+  fd = msgget(key, IPC_CREAT|0606);
+  for(i = 0; i < 18; i++){
+    if(msgrcv(fd, &mobj, 101, -4, 0) > 0)
+      printf("%s\n", mobj.mtext);
+    else{
+      printf("not queue\n");
+      exit(0);
+    }
+  }
+}
---
+++ server3.cpp
@@ -0,0 +1,21 @@
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <cstring>
+
+struct mbuf{
+  long mtype;
+  char mtext[100] = "";
+}mobj={3, "server3"};
+
+int main(){
+  int fd, key;
+  key = ftok("queue", 1);
+  fd = msgget(key, IPC_CREAT|0606);
+  for(int i = 0; i < 6; i++){
+    msgsnd(fd, &mobj, strlen(mobj.mtext)+1, 0);
+  }
+  exit(0);
+}
